796eae73afd390a5dfe4fd4f2c4b4af56603debe
gerodrus <gerodrus@gmail.com>
2017-06-05 10:50:17 +0300

    Added markerless camera to FirstScene

---
+++ Assets/Scripts/PlaceMarkerlessObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kudan.AR;
+
+
+public class PlaceMarkerlessObject : MonoBehaviour
+{
+    public KudanTracker _kudanTracker;
+    
+    public void PlaceClick()
+    {
+        Vector3 position;
+        Quaternion rotation;
+        
+        _kudanTracker.FloorPlaceGetPose(out position, out rotation);
+        _kudanTracker.ArbiTrackStart(position, rotation);
+    }
+}
